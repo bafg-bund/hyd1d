dd6c58ca9dbdc61487fe3571aecec6a437855c3f
Arnd Weber <arnd.weber@bafg.de>
2018-05-24 19:41:44 +0200

    delete preview html file

--- _documentation.sh
+++ _documentation.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env 
-Rscript -e 'rmarkdown::render("README.Rmd", "github_document", "README.md")'
+Rscript -e 'rmarkdown::render("README.Rmd", output_format = "github_document", output_file = "README.md", clean = TRUE)'
+Rscript -e 'unlink("README.html", force = TRUE)'
 Rscript -e 'pkgdown::build_site(".", examples = TRUE, preview = FALSE)'
