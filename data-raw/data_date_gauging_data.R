5b68dd31155936cbca045f7858e28d28766a9ca9
Arnd Weber <arnd.weber@bafg.de>
2018-05-24 19:26:02 +0200

    don't replace existing dataset

--- data-raw/data_date_gauging_data.R
+++ data-raw/data_date_gauging_data.R
@@ -3,8 +3,12 @@
 date_gauging_data <- Sys.Date()
 
 # store date_gauging_data as external dataset
-devtools::use_data(date_gauging_data, pkg = ".", overwrite = TRUE, 
-                   compress = "bzip2")
+if (!(file.exists("data/date_gauging_data.rda"))){
+    devtools::use_data(date_gauging_data, pkg = ".", overwrite = TRUE, 
+                       compress = "bzip2")
+} else {
+    print("data/date_gauging_data.rda exists already")
+}
 
 # clean up
 rm(date_gauging_data)
