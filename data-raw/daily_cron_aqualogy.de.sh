4b02ff53a34767c788064de977f1884cf6fe2102
Arnd Weber <arnd.weber@bafg.de>
2018-06-20 15:53:59 +0200

    use path variable $hyd1d; use R 3.5.0 from i4 module environment

--- data-raw/daily_cron_aqualogy.de.sh
+++ data-raw/daily_cron_aqualogy.de.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
-cd /home/WeberA/BfG/hyd1d/
+# download present package repository
+cd $hyd1d
 git pull
+
+# use R 3.5.0 from the i4 module environment
+source /etc/profile.d/modules.sh
+module add i4/applications/R-3.5.0
+
+# run the daily scripts
 Rscript _install.R
 Rscript _build.R
 Rscript data-raw/daily_pegelonline2gauging_data.R
 Rscript data-raw/daily_df.gauging_data.R
-chown -R WeberA:WeberA /home/WeberA/BfG/hyd1d/
+chown -R arnd:arnd $hyd1d
 
 # sync hyd1d website
 export OPTS="-v --recursive --delete --times --no-implied-dirs --iconv=utf8"
-rsync $OPTS /home/WeberA/BfG/hyd1d/public/3.4.4/ /var/www/hyd1d
+rsync $OPTS $hyd1d/public/3.5.0/ /var/www/hyd1d
 chown -R www-data:www-data /var/www/hyd1d
 chown root:www-data /var/www/hyd1d
 
