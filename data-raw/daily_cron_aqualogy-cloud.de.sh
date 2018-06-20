4937b8fcc6b9f4a220183ccdbad4fd510640d879
Arnd Weber <arnd.weber@bafg.de>
2018-06-20 14:29:26 +0200

    sync R version specific public directory

--- data-raw/daily_cron_aqualogy-cloud.de.sh
+++ data-raw/daily_cron_aqualogy-cloud.de.sh
@@ -9,7 +9,7 @@ chown -R WeberA:WeberA /home/WeberA/hyd1d/
 
 # sync hyd1d website
 export OPTS="-v --recursive --delete --times --no-implied-dirs --iconv=utf8"
-rsync $OPTS /home/WeberA/hyd1d/public/ /var/www/vhosts/lvps46-163-72-150.dedicated.hosteurope.de/aqualogy-cloud.de/hyd1d
+rsync $OPTS /home/WeberA/hyd1d/public/3.5.0/ /var/www/vhosts/lvps46-163-72-150.dedicated.hosteurope.de/aqualogy-cloud.de/hyd1d
 chown -R apache:apache /var/www/vhosts/lvps46-163-72-150.dedicated.hosteurope.de/aqualogy-cloud.de/hyd1d
 
 # exit
