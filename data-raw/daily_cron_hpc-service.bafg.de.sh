a945417b7d4351b4259d9308eb735236228a165b
Arnd Weber <arnd.weber@bafg.de>
2018-06-20 14:35:53 +0200

    make the rsync of public/ R version specific

--- data-raw/daily_cron_hpc-service.bafg.de.sh
+++ data-raw/daily_cron_hpc-service.bafg.de.sh
@@ -9,7 +9,7 @@ chown -R WeberA:users /srv/cifs-mounts/WeberA_home/WeberA/hyd1d
 
 # sync hyd1d website
 export OPTS="-v --recursive --delete --times --no-implied-dirs --iconv=utf8"
-rsync $OPTS /srv/cifs-mounts/WeberA_home/WeberA/hyd1d/public/ /home/WeberA/public_html/hyd1d
+rsync $OPTS /srv/cifs-mounts/WeberA_home/WeberA/hyd1d/public/3.5.0/ /home/WeberA/public_html/hyd1d
 chown -R WeberA:users /home/WeberA/public_html/hyd1d
 
 # exit
