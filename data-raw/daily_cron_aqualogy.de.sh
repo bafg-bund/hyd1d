1c24f2e8f790d21617daa31402996150cc2659d5
Arnd Weber <arnd.weber@bafg.de>
2018-06-21 19:02:29 +0200

    deactivate daily _install.R, _build.R and website upload

--- data-raw/daily_cron_aqualogy.de.sh
+++ data-raw/daily_cron_aqualogy.de.sh
@@ -8,17 +8,17 @@ source /etc/profile.d/modules.sh
 module add i4/applications/R-3.5.0
 
 # run the daily scripts
-Rscript _install.R
-Rscript _build.R
+#Rscript _install.R
+#Rscript _build.R
 Rscript data-raw/daily_pegelonline2gauging_data.R
 Rscript data-raw/daily_df.gauging_data.R
 chown -R arnd:arnd $hyd1d
 
 # sync hyd1d website
-export OPTS="-v --recursive --delete --times --no-implied-dirs --iconv=utf8"
-rsync $OPTS $hyd1d/public/3.5.0/ /var/www/hyd1d
-chown -R www-data:www-data /var/www/hyd1d
-chown root:www-data /var/www/hyd1d
+#export OPTS="-v --recursive --delete --times --no-implied-dirs --iconv=utf8"
+#rsync $OPTS $hyd1d/public/3.5.0/ /var/www/hyd1d
+#chown -R www-data:www-data /var/www/hyd1d
+#chown root:www-data /var/www/hyd1d
 
 # exit
 exit 0
