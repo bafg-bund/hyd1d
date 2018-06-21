0e513b675caa88e97b4628210bc3fbac57afcd4f
Arnd Weber <arnd.weber@bafg.de>
2018-06-21 10:58:02 +0200

    explicitly set permissions for the content of public_html/hyd1d/

--- data-raw/daily_cron_hpc-service.bafg.de.sh
+++ data-raw/daily_cron_hpc-service.bafg.de.sh
@@ -11,6 +11,8 @@ chown -R WeberA:users /srv/cifs-mounts/WeberA_home/WeberA/hyd1d
 export OPTS="-v --recursive --delete --times --no-implied-dirs --iconv=utf8"
 rsync $OPTS /srv/cifs-mounts/WeberA_home/WeberA/hyd1d/public/3.5.0/ /home/WeberA/public_html/hyd1d
 chown -R WeberA:users /home/WeberA/public_html/hyd1d
+find /home/WeberA/public_html/hyd1d/ -type f -print0 | xargs -0 chmod 0644
+find /home/WeberA/public_html/hyd1d/ -type d -print0 | xargs -0 chmod 0755
 
 # exit
 exit 0
