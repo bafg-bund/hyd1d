1385a43cc45f6e55595479399e2b0f3da2929074
Arnd Weber <arnd.weber@bafg.de>
2018-07-06 20:43:55 +0200

    add R package 'revealjs' to create presentations

--- _install.R
+++ _install.R
@@ -2,9 +2,9 @@
 # _install.R
 #
 # author: arnd.weber@bafg.de
-# date:   30.05.2018
+# date:   06.07.2018
 #
-# purpose: 
+# purpose:
 #   - install R packages required for the CI jobs
 #   - install the repository version of hyd1d
 #
@@ -23,7 +23,7 @@ dir.create(lib, verbose, TRUE)
 # ROracle (>= 1.1-1) needs an Oracle (Instant)Client
 packages <- c("RJSONIO", "RCurl", "plotrix", "Rdpack", "DBI", "ROracle", 
               "RPostgreSQL", "testthat", "knitr", "rmarkdown", "stringr", 
-              "devtools", "pkgdown", "roxygen2")
+              "devtools", "pkgdown", "roxygen2", "revealjs")
 
 for (a_package in packages) {
     if (! (a_package %in% installed.packages(lib.loc = lib)[, "Package"])) {
