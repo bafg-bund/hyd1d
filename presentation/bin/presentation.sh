c0989262b858c0a16b7cfca08249385aaedb96c4
Arnd Weber <arnd.weber@bafg.de>
2018-07-09 10:49:18 +0200

    final steps to move the presentation from vignettes/ to new presentation/ directory

--- presentation/bin/presentation.sh
+++ presentation/bin/presentation.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
-cd /srv/cifs-mounts/WeberA_home/WeberA/hyd1d
+cd /home/WeberA/hyd1d_tmp
 
 # load modules
 module add i4/applications/R-3.5.0
 module add i4/applications/pandoc-2.1.1
 
 # render the presentation as html
-Rscript -e "rmarkdown::render('presentation/presentation.Rmd', output_file = 'index.html', output_dir = '/home/WeberA/public_html/hyd1d/presentation/')"
+Rscript -e "rmarkdown::render('presentation/presentation.Rmd', output_file = 'presentation_DE.html', output_dir = '/home/WeberA/public_html/hyd1d/articles/')"
 
 # and as pdf
-#Rscript -e "rmarkdown::render('presentation/presentation.Rmd', output_format = 'pdf_document', output_file = 'presentation.pdf', output_dir = '/home/WeberA/public_html/hyd1d/presentation/')"
+#Rscript -e "rmarkdown::render('presentation/presentation.Rmd', output_format = 'pdf_document', output_file = 'presentation_DE.pdf', output_dir = '/home/WeberA/public_html/hyd1d/articles/')"
 
 # copy external image files
-cp -rf presentation/*.png /home/WeberA/public_html/hyd1d/presentation/
-cp -rf presentation/*.css /home/WeberA/public_html/hyd1d/presentation/
+cp -rf presentation/*.png /home/WeberA/public_html/hyd1d/articles/
+cp -rf presentation/*.css /home/WeberA/public_html/hyd1d/articles/
 
 # set SE linux permissions
 chcon -R -t httpd_user_content_t /home/WeberA/public_html/
