c87ae9e07fec76e484eb1bf32040f6e21df567e8
Arnd Weber <arnd.weber@bafg.de>
2018-08-01 17:34:55 +0200

    updated R; create pdf output; copy *.mp4 files

--- presentation/bin/presentation.sh
+++ presentation/bin/presentation.sh
@@ -2,18 +2,19 @@
 cd /home/WeberA/hyd1d_tmp
 
 # load modules
-module add i4/applications/R-3.5.0
+module add i4/applications/R-3.5.1
 module add i4/applications/pandoc-2.1.1
 
 # render the presentation as html
 Rscript -e "rmarkdown::render('presentation/presentation.Rmd', output_file = 'presentation_DE.html', output_dir = '/home/WeberA/public_html/hyd1d/articles/')"
 
 # and as pdf
-#Rscript -e "rmarkdown::render('presentation/presentation.Rmd', output_format = 'pdf_document', output_file = 'presentation_DE.pdf', output_dir = '/home/WeberA/public_html/hyd1d/articles/')"
+Rscript -e "rmarkdown::render('presentation/presentation.Rmd', output_format = 'pdf_document', output_file = 'presentation_hyd1d_DE.pdf', output_dir = '/home/WeberA/public_html/hyd1d/downloads/')"
 
 # copy external image files
 cp -rf presentation/*.png /home/WeberA/public_html/hyd1d/articles/
 cp -rf presentation/*.css /home/WeberA/public_html/hyd1d/articles/
+cp -rf presentation/*.mp4 /home/WeberA/public_html/hyd1d/articles/
 
 # set SE linux permissions
 chcon -R -t httpd_user_content_t /home/WeberA/public_html/
