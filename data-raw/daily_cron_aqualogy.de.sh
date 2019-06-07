0c00d5359224ae40309ca7ebcb3a6af3bb1a07ee
Arnd Weber <arnd.weber@bafg.de>
2019-06-07 09:53:24 +0200

    set permissions for hyd1d-document root

--- data-raw/daily_cron_aqualogy.de.sh
+++ data-raw/daily_cron_aqualogy.de.sh
@@ -15,10 +15,17 @@ Rscript data-raw/daily_df.gauging_data.R
 chown -R arnd:arnd $hyd1d
 
 # sync hyd1d website
+export TO=/var/www/hyd1d
 export OPTS="-v --recursive --delete --times --no-implied-dirs --iconv=utf8"
-rsync $OPTS $hyd1d/public/$R_VERSION/ /var/www/hyd1d
-chown -R www-data:www-data /var/www/hyd1d
-chown root:www-data /var/www/hyd1d
+rsync $OPTS $hyd1d/public/$R_VERSION/ $TO
+chown -R www-data:www-data $TO
+chown root:www-data $TO
+
+# set permissions
+chown -R www-data:www-data $TO
+chown root:www-data $TO
+find ${TO}/ -type f -print0 | xargs -0 chmod 0644
+find ${TO}/ -type d -print0 | xargs -0 chmod 0750
 
 # exit
 exit 0
