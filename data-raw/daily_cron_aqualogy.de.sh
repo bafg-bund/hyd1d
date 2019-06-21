fa927be9c8bb051289f85b5761b120c287b8d9d6
Arnd Weber <arnd.weber@bafg.de>
2019-06-21 10:50:22 +0200

    if $USER==root, sync products to webserver

--- data-raw/daily_cron_aqualogy.de.sh
+++ data-raw/daily_cron_aqualogy.de.sh
@@ -14,18 +14,34 @@ Rscript data-raw/daily_pegelonline2gauging_data.R
 Rscript data-raw/daily_df.gauging_data.R
 chown -R arnd:arnd $hyd1d
 
-# sync hyd1d website
-export TO=/var/www/hyd1d
-export OPTS="-v --recursive --delete --times --no-implied-dirs --iconv=utf8"
-rsync $OPTS $hyd1d/public/$R_VERSION/ $TO
-chown -R www-data:www-data $TO
-chown root:www-data $TO
-
-# set permissions
-chown -R www-data:www-data $TO
-chown root:www-data $TO
-find ${TO}/ -type f -print0 | xargs -0 chmod 0644
-find ${TO}/ -type d -print0 | xargs -0 chmod 0750
+# check user, then sync
+if [ "$USER" == "root" ]
+  then
+    # sync hyd1d website
+    export FROM=$hyd1d/public/$R_VERSION
+    export TO=/var/www/hyd1d
+    export OPTS="-v --recursive --delete --times --no-implied-dirs --iconv=utf8"
+    rsync $OPTS $FROM $TO
+    chown -R www-data:www-data $TO
+    chown root:www-data $TO
+    
+    # set permissions
+    chown -R www-data:www-data $TO
+    chown root:www-data $TO
+    find ${TO}/ -type f -print0 | xargs -0 chmod 0644
+    find ${TO}/ -type d -print0 | xargs -0 chmod 0750
+    
+    # sync df.gauging_data_latest.RDS
+    export PUBLIC=/var/www/wordpress/wp-content/uploads/bfg
+    export GD=$PUBLIC/df.gauging_data_latest.RDS
+    export GD_v2=$PUBLIC/df.gauging_data_latest_v2.RDS
+    cp $FROM/downloads/df.gauging_data_latest.RDS $GD
+    cp $FROM/downloads/df.gauging_data_latest_v2.RDS $GD_v2
+    chown www-data:www-data $GD
+    chown www-data:www-data $GD_v2
+    chmod 640 $GD
+    chmod 640 $GD_v2
+fi
 
 # exit
 exit 0
