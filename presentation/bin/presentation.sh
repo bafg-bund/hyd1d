d043393a92efdc84a26559d293f32994abb3fef8
Arnd Weber <arnd.weber@bafg.de>
2019-11-05 11:59:33 +0100

    renaming R package hydflood3 to hydflood

--- presentation/bin/presentation.sh
+++ presentation/bin/presentation.sh
@@ -2,12 +2,12 @@
 cd /home/WeberA/hyd1d_tmp
 
 # load modules
-module add i4/applications/R-3.5.1
-module add i4/applications/pandoc-2.2.3.2
+module add i4/R/OS
+module add i4/pandoc/2.3.2
 
 # render the presentation as html
 Rscript -e "rmarkdown::render('presentation/presentation.Rmd', output_file = 'presentation_DE.html', output_dir = '/home/WeberA/public_html/hyd1d/articles/')"
-Rscript -e "rmarkdown::render('presentation/presentation.Rmd', output_file = 'presentation_DE.html', output_dir = '/home/WeberA/public_html/hydflood3/articles/')"
+Rscript -e "rmarkdown::render('presentation/presentation.Rmd', output_file = 'presentation_DE.html', output_dir = '/home/WeberA/public_html/hydflood/articles/')"
 
 # and as pdf
 Rscript -e "rmarkdown::render('presentation/presentation.Rmd', output_format = 'pdf_document', output_file = 'presentation_hyd1d_DE.pdf', output_dir = '/home/WeberA/public_html/hyd1d/downloads/')"
@@ -16,9 +16,9 @@ Rscript -e "rmarkdown::render('presentation/presentation.Rmd', output_format = '
 cp -rf presentation/*.png /home/WeberA/public_html/hyd1d/articles/
 cp -rf presentation/*.css /home/WeberA/public_html/hyd1d/articles/
 cp -rf presentation/*.mp4 /home/WeberA/public_html/hyd1d/articles/
-cp -rf presentation/*.png /home/WeberA/public_html/hydflood3/articles/
-cp -rf presentation/*.css /home/WeberA/public_html/hydflood3/articles/
-cp -rf presentation/*.mp4 /home/WeberA/public_html/hydflood3/articles/
+cp -rf presentation/*.png /home/WeberA/public_html/hydflood/articles/
+cp -rf presentation/*.css /home/WeberA/public_html/hydflood/articles/
+cp -rf presentation/*.mp4 /home/WeberA/public_html/hydflood/articles/
 
 # set SE linux permissions
 chcon -R -t httpd_user_content_t /home/WeberA/public_html/
