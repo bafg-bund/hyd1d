829b06537bac7bedb2a52df9d9d35013cddaa4c7
Arnd Weber <arnd.weber@bafg.de>
2022-11-24 09:00:31 +0100

    update external version numbers

--- _build.R
+++ _build.R
@@ -238,7 +238,7 @@ write(" web", stdout())
 
 host <- Sys.info()["nodename"]
 user <- Sys.info()["user"]
-if (host == "r.bafg.de" & user == "WeberA" & R_version == "4.2.1") {
+if (host == "pvil-r" & user == "WeberA" & R_version == "4.2.2") {
     # copy html output to ~/public_html
     system(paste0("cp -rp ", public, "* /home/", user, "/public_html/hyd1d/"))
     system("permissions_html")
