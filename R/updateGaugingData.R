344085c504168519bf558d0be3254f84347be386
Arnd Weber <arnd.weber@bafg.de>
2022-12-21 17:01:23 +0100

    determine local storage location through options("hyd1d.datadir")

--- R/updateGaugingData.R
+++ R/updateGaugingData.R
@@ -16,9 +16,8 @@
 #'   \code{\link{df.gauging_data}} has been downloaded.
 #' 
 #' @examples
-#' \dontrun{
-#' updateGaugingData(as.Date("2016-12-21"))
-#' }
+#'   options("hyd1d.datadir" = tempdir())
+#'   updateGaugingData(as.Date("2016-12-21"))
 #'
 #' @export
 #' 
@@ -39,15 +38,15 @@ updateGaugingData <- function(x) {
     
     # set relevant DB variables
     if (utils::compareVersion(as.character(getRversion()), "3.5.0") < 0) {
-        file_date <- paste0(path.expand('~'), "/.hyd1d/date_gauging_data_v2.RD",
+        file_date <- paste0(options()$hyd1d.datadir, "/date_gauging_data_v2.RD",
                             "S")
-        file_data <- paste0(path.expand('~'), "/.hyd1d/df.gauging_data_latest_",
+        file_data <- paste0(options()$hyd1d.datadir, "/df.gauging_data_latest_",
                             "v2.RDS")
         url <- paste0("https://hyd1d.bafg.de/downloads/df.gauging_data_latest_",
                       "v2.RDS")
     } else {
-        file_date <- paste0(path.expand('~'), "/.hyd1d/date_gauging_data.RDS")
-        file_data <- paste0(path.expand('~'), "/.hyd1d/df.gauging_data_latest.",
+        file_date <- paste0(options()$hyd1d.datadir, "/date_gauging_data.RDS")
+        file_data <- paste0(options()$hyd1d.datadir, "/df.gauging_data_latest.",
                             "RDS")
         url <- paste0("https://hyd1d.bafg.de/downloads/df.gauging_data_latest.",
                       "RDS")
@@ -58,13 +57,17 @@ updateGaugingData <- function(x) {
        (!file.exists(file_date) & !file.exists(file_data))) {
         
         # download the df.gauging_data.RDS
-        dir.create(paste0(path.expand('~'), "/.hyd1d"), FALSE, TRUE, "0777")
-        utils::download.file(url, file_data, quiet = TRUE, method = "curl")
-        
-        # store todays date
-        date_gauging_data <- Sys.Date()
-        saveRDS(date_gauging_data, file = file_date)
-        .db_updated <<- TRUE
+        dir.create(options()$hyd1d.datadir, FALSE, TRUE, "0777")
+        tryCatch({
+            utils::download.file(url, file_data, quiet = TRUE, method = "curl")
+            
+            # store todays date
+            date_gauging_data <- Sys.Date()
+            saveRDS(date_gauging_data, file = file_date)
+            .db_updated <<- TRUE
+        }, error = function(e){paste0("It was not possible to update the gaugi",
+                                      "ng data. Try again later!")}
+        )
         
         if (file.exists(file_data) & file.exists(file_date) & 
             (file.info(file_date)$mtime > Sys.time() - 5)) {
